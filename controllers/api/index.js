d70adb07804b84e33e1591b24a4c45add0e0f4ec
ykuang321 <112137899+ykuang321@users.noreply.github.com>
2022-12-26 08:01:49 -0600

    add backend logic for count

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const inventoryRoutes = require('./inventoryRoutes');
 const userRoutes = require('./userRoutes');
 const itemRoutes = require('./itemManagementRoutes');
+const countRoutes = require('./countRoutes')
 
 router.use('/inventory', inventoryRoutes);
 router.use('/users', userRoutes);
 router.use('/itemManagement', itemRoutes);
+router.use('/count', countRoutes);
 
 module.exports = router;
