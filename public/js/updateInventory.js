4e0f39d23eaf5bdcd60b379a99da47270384c3cd
ykuang321 <112137899+ykuang321@users.noreply.github.com>
2023-01-01 12:31:04 -0600

    modify the price type so it will show 99.99, and add logic to validate quantity input when user update the inventory

--- public/js/updateInventory.js
+++ public/js/updateInventory.js
@@ -3,26 +3,33 @@ const updateInventory = async (event) => {
 
   if (event.target.hasAttribute('data-id')) {
 
-  // Collect data from user input
-  const quantity = document.querySelector('#new-quantity').value.trim();
-  const id = event.target.getAttribute('data-id');
+    // Collect data from user input
+    const quantity = document.querySelector('#new-quantity').value.trim();
+    const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/inventory/${id}`, {
-      method: 'PUT',
-      body: JSON.stringify({
-        inventory_id: id,
-        quantity,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    if (quantity >= 0){
+      const response = await fetch(`/api/inventory/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          inventory_id: id,
+          quantity,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-    if (response.ok) {
-      document.location.replace('/api/inventory');
-    } else {
-      alert('Failed to update inventory quantity');
-    }
+      if (response.ok) {
+        document.location.replace('/api/inventory');
+      } else {
+        alert('Failed to update inventory quantity');
+      }
+    };
+        
+    if (quantity < 0){
+      alert("Invalid quantity input, please try again");
+    };
+    
   }
 };
 
